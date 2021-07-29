262aee9c53393b13495f87abd13a66b3c3408034
Niki <niki.minev@yahoo.com>
2021-07-29 21:50:15 +0300

    Adding comments to remember problem with EF Identity

--- Database/DatabaseStartUp.cs
+++ Database/DatabaseStartUp.cs
@@ -19,6 +19,10 @@ namespace Database
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            //here I found an interesting reason to dislike .net core identity. Turns out that because I use their UI it overrides this and this is pointless for client side registration.
+            //an option to fix it is to use scafolding but I dont want to do that because that will install the .net core EF package to my web project.
+            //so in the future I'll either not use .net core idetity, use a seperate project for its UI and its generated scafolds or submit to the password limitations.
+            //for now since im out of time I choose the third option.
             services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequireDigit = false;
